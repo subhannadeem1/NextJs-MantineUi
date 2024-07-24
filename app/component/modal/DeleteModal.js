abaf9233bfe83bb90325d3a8087f90a7e39490d5
subhan1212 <70110442@student.uol.edu.pk>
2024-07-24 10:55:42 +0500

    Supabase integration

--- app/component/modal/DeleteModal.js
+++ app/component/modal/DeleteModal.js
@@ -1,7 +1,7 @@
 import { Modal, Button, Group, Text, Box, Paper } from '@mantine/core';
 import { IconQuestionMark } from '@tabler/icons-react';
 
-const DeleteModal = ({ isOpen, onClose, onDelete }) => {
+const DeleteModal = ({ isOpen, onClose, onDelete, user }) => {
   return (
     <Modal
       opened={isOpen}
@@ -11,38 +11,42 @@ const DeleteModal = ({ isOpen, onClose, onDelete }) => {
       padding="lg"
       radius={15}
     >
-      <Paper   >
-      <Box>
-        <Box position="center" ml={"382px"}>
-          <IconQuestionMark stroke={2} />
+      <Paper>
+        <Box>
+          <Box position="center" ml={"382px"}>
+            <IconQuestionMark stroke={2} />
+          </Box>
+          <Text size="xl" fw={700} pt={"30px"} weight={500} align="center">
+            Confirm
+          </Text>
+          <Text align="center" fw={400} pt={"16px"}>
+            Are you sure you want to delete {user ? `${user.firstName} ${user.lastName}` : 'this user'}?
+          </Text>
+          <Group position="center" pt={"40px"} spacing="lg">
+            <Button
+              variant="outline"
+              color="green"
+              onClick={onClose}
+              mb={"60px"}
+              ml={"40px"}
+              style={{ width: 345, height: 60, borderRadius: "15px" }}
+            >
+              No
+            </Button>
+            <Button
+  color="green"
+  onClick={() => {
+    if (user) {
+      onDelete(user); // Ensure `user` is passed correctly
+    }
+  }}
+  mb={"60px"}
+  style={{ width: 345, height: 60, borderRadius: "15px" }}
+>
+  Yes
+</Button>
+          </Group>
         </Box>
-        <Text size="xl" fw={700} pt={"30px"} weight={500} align="center">
-          Confirm
-        </Text>
-        <Text align="center" fw={400} pt={"16px"}>
-          Are you sure you want to delete Subhan Nadeem?
-        </Text>
-        <Group position="center" pt={"40px"} spacing="lg">
-          <Button
-            variant="outline"
-            color="green"
-            onClick={onClose}
-            mb={"60px"}
-            ml={"40px"}
-            style={{ width: 345, height: 60, borderRadius: "15px" }}
-          >
-            No
-          </Button>
-          <Button
-            color="green"
-            onClick={onDelete}
-            mb={"60px"}
-            style={{ width: 345, height: 60, borderRadius: "15px" }}
-          >
-            Yes
-          </Button>
-        </Group>
-      </Box>
       </Paper>
     </Modal>
   );
